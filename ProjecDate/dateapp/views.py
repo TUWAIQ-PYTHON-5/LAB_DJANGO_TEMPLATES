01102b2cf2150c0753a3096d330741e1b83c51d8
Faimubarak <102253601+Faimubarak@users.noreply.github.com>
2023-02-01 11:36:31 +0300

    ub

--- ProjecDate/dateapp/views.py
+++ ProjecDate/dateapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest, HttpResponse
+from datetime import date
+import random
+import string
 # Create your views here.
+
+
+def today_page(request : HttpRequest):
+    today = date.today()
+    context ={"today": today}
+    return render(request , "dateapp/today.html", context)
+
+
+def random_pass(request :HttpRequest):
+    random_pass= random.choice(string.ascii_letters + string.digits+string.punctuation)
+    random_pass_string = "".join(random_pass)
+    return render(request, "dateapp/randompass.html", {'password': random_pass_string})
+
+
+def favoritlist(request: HttpRequest):
+
+    return render(request,'dateapp/fav.html')
