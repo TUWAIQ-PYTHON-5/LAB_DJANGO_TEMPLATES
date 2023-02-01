5a0d702bbae7f011bc67deb92c1d1cfd7fbf1f7a
atheer <atheeralq0@gmail.com>
2023-02-01 11:52:12 +0300

    lab templates end

--- Start_tody/tody/views.py
+++ Start_tody/tody/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse
-from datetime import  date
+import  datetime
 import random
 import string
 
 # Create your views here.
 def date(request : HttpRequest):
-    context={"today" : date.today()}
+    today=datetime.date.today()
+    context={"today" : today}
     return render(request,"Start_tody/index.html", context)
 
 def password(request : HttpRequest):
     random_pass = random.choices(string.ascii_letters + string.digits + string.punctuation, k=15)
     random_pass_string = "".join(random_pass)
-    return render(request,"Start_tody/index2.html", {"passwrd",random_pass_string})
+    return render(request,"Start_tody/index2.html", {"passwrd" : random_pass_string})
 
 def games(request : HttpRequest):
     return render(request,"Start_tody/index3.html")
